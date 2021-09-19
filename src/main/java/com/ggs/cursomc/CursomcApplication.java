c10e60edc128e09bcadd6937baacc6d3d730f118
GIVALDOGS <givaldowanda@gmail.com>
2021-09-19 00:09:46 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/ggs/cursomc/CursomcApplication.java
+++ src/main/java/com/ggs/cursomc/CursomcApplication.java
@@ -1,6 +1,5 @@
 package com.ggs.cursomc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ggs.cursomc.domain.Categoria;
+import com.ggs.cursomc.domain.Produto;
 import com.ggs.cursomc.repositories.CategoriaRepository;
+import com.ggs.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class CursomcApplication implements  CommandLineRunner{
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -24,13 +28,24 @@ public class CursomcApplication implements  CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
-		
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
-		
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 	}
 
 }
