735ec24de7eb2a4b0b40076305557660d776f566
GIVALDOGS <givaldowanda@gmail.com>
2021-10-03 21:37:48 -0300

    Endpoint /clientes/{id}

---
+++ src/main/java/com/ggs/cursomc/resources/ClienteResource.java
@@ -0,0 +1,39 @@
+package com.ggs.cursomc.resources;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ggs.cursomc.domain.Cliente;
+import com.ggs.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
+		Optional<Cliente> obj = service.buscar(id);
+		
+		/**
+		Cliente cat1 = new Cliente (1, "Informatica");
+		Cliente cat2 = new Cliente (2, "Escritorio");
+		
+		List<Cliente> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		*/
+		
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
---
+++ src/main/java/com/ggs/cursomc/services/ClienteService.java
@@ -0,0 +1,29 @@
+package com.ggs.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ggs.cursomc.domain.Cliente;
+import com.ggs.cursomc.repositories.ClienteRepository;
+import com.ggs.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Optional<Cliente> buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		
+		//if (obj == null) {
+		//	throw new ObjectNotFoundException(
+		//			"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName());
+		//}		
+		//return obj;
+		return Optional.of(obj.orElseThrow(() -> new ObjectNotFoundException (
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())));
+	}
+}
