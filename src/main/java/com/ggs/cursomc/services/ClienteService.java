4eb4ae86294c2d9cf7d222887bb9e1dabf9de036
GIVALDOGS <givaldowanda@gmail.com>
2022-01-13 20:10:45 -0300

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/ggs/cursomc/services/ClienteService.java
+++ src/main/java/com/ggs/cursomc/services/ClienteService.java
@@ -58,7 +58,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Nao e possivel excluir um Cliente porque ha entidades relacionadas");
+			throw new DataIntegrityException("Nao e possivel excluir um Cliente porque ha pedidos relacionadas");
 		}
 
 	}
