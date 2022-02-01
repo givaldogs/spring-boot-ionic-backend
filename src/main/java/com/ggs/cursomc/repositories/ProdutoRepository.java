b44863ea71c0f9111588ea9ec4845ad4b07dbc71
GIVALDOGS <givaldowanda@gmail.com>
2022-02-01 20:56:15 -0300

    Busca de pedidos por nome e categorias - PARTE 2

--- src/main/java/com/ggs/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/ggs/cursomc/repositories/ProdutoRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +16,10 @@ import com.ggs.cursomc.domain.Produto;
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
  
 	@Transactional(readOnly=true)
+	// se for usar o @query e metodo searc , tem que executar o ctrl shift o , para importar objetos
+	//
+	//@Query("Select DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	//Page<Produto> search(@Param("nome") String nome, @Param("categorias")List<Categoria> categorias, Pageable pageRequest);
 	@Query("Select DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
-	Page<Produto> search(@Param("nome") String nome, @Param("categorias")List<Categoria> categorias, Pageable pageRequest);
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
