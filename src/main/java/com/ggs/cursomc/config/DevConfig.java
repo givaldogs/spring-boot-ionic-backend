10a1045308032fabe1e043e5fe572807e57bbced
GIVALDOGS <givaldowanda@gmail.com>
2022-03-03 11:13:59 -0300

    Inserindo Pedido

---
+++ src/main/java/com/ggs/cursomc/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.ggs.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.ggs.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.profiles.active}")
+	private String strategy;
+	
+	@Bean 
+	public boolean instantiateDataBase() throws ParseException {
+		//if (!"create".equals(strategy)) {
+		//	return false;
+		//}
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+}
